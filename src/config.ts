542a48639534c06b307895962396bbf02e8b098c
ClaireWhere <clairewhere4@gmail.com>
2023-12-21 23:19:22 -0500

    fix: add .com to youtube url

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const config = {
     "githubUsername": "ClaireWhere",
     "discordUsername": "ClaireWhere",
     "websiteURL": "https://www.clairewhere.com",
-    "youtubeURL": "https://youtube/com/@ClaireWhere",
+    "youtubeURL": "https://youtube.com/@ClaireWhere",
     "twitchURL": "https://twitch.tv/ClaireWhere",
     "discordURL": "https://discord.com/users/clairewhere",
     "badgeColor": "D1A3FF",
